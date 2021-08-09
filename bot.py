f9332afe3ba6cc0037d33f2530df6b2d6d891a5f
MahatKC <mkarvat@gmail.com>
2021-08-09 11:29:25 -0300

    bot connecting and getting member list

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild: \n' 
+        f'{guild.name} (id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    
+client.run(TOKEN)
